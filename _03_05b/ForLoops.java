a286877fc67681f889666bb6a252a838b36951f7
Matthew Clark <matt314hew@gmail.com>
2024-01-12 17:59:13 +0000

    completed the exercises

--- _03_05b/ForLoops.java
+++ _03_05b/ForLoops.java
@@ -4,11 +4,19 @@ public class ForLoops {
 
   public static void main(String[] args) {
     // Write a for loop that prints out the phrase "I love for loops" 5 times
+    for (int idx = 0; idx < 5; idx++) {
+      System.out.println("I love for loops");
+    }
 
     // Write a for loop that prints out the numbers 1 to 10
+    for (int idx = 1; idx <= 10; idx++) {
+      System.out.println(idx);
+    }
 
     // Write a for loop that prints out the numbers 10 to 1
-
+    for (int idx = 10; idx > 0; idx--) {
+      System.out.println(idx);
+    }
   }
 
 }
